fe68f059efdd6b049a865d986f653d444e43ea51
Bezobrazz <kostenko.uvd@gmail.com>
2022-07-18 08:37:51 +0300

    sale item added

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-  $(".banner-section__slider").slick({});
+  $(".banner-section__slider").slick({
+    dots: true,
+    prevArrow:
+      '<button class="banner-section__slider-btn banner-section__slider-btnPrev"><img src="icons/prev.svg" alt="arrow"></button>',
+    nextArrow:
+      '<button class="banner-section__slider-btn banner-section__slider-btnNext"><img src="icons/next.svg" alt="arrow"></button>',
+  });
 });
